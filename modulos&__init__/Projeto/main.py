b10b896a70203227c9bc2a98e6f423bf01304661
abraaohenriquevasconcelos <henrique.vscncls@gmail.com>
2017-02-13 11:25:40 -0200

    ...

---
+++ modulos&__init__/Projeto/main.py
@@ -0,0 +1,11 @@
+#from módulo1.móduloLegal import imprime1, imprime2
+
+from módulo1 import imprime1, imprime2
+
+def main():
+    imprime1()
+    imprime2()
+
+
+if __name__ == "__main__":
+    main()
---
+++ modulos&__init__/Projeto/módulo1/móduloLegal.py
@@ -0,0 +1,9 @@
+def imprime1():
+    print("imprime1")
+
+def imprime2():
+    print("imprime2")
+
+def imprime3():
+    print("imprime3")
+
